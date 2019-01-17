58ce7a7509a32245c4630a532f0e355773f6e6e7
Steven Bundy <sbundy.training@gmail.com>
2019-01-17 17:54:41 -0600

    Web Programming Lesson 11 if decision statements

---
+++ CS-ASP_011/CS-ASP_011/Default.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CS_ASP_011.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Is
+            <asp:TextBox ID="firstTextBox" runat="server"></asp:TextBox>
+&nbsp;equal to
+            <asp:TextBox ID="secondTextBox" runat="server"></asp:TextBox>
+&nbsp;?<br />
+            <br />
+            <br />
+            <asp:CheckBox ID="coolCheckBox" runat="server" Text="Are you cool?" />
+            <br />
+            <br />
+            If you could only eat one food for the rest of your life what would you choose?<br />
+            <asp:RadioButton ID="pizzaRadioButton" runat="server" GroupName="FoodGroup" Text="Pizza" />
+            <br />
+            <asp:RadioButton ID="crabLegsRadioButton" runat="server" GroupName="FoodGroup" Text="Crab Legs" />
+            <br />
+            <asp:RadioButton ID="cheeseburgerRadioButton" runat="server" GroupName="FoodGroup" Text="Cheeseburger" />
+            <br />
+            <br />
+            <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" style="height: 26px" Text="Ok" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server" Text="resultLabel"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CS-ASP_011/CS-ASP_011/Default.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_011
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            resultLabel.Text = "";
+
+            //if (firstTextBox.Text == secondTextBox.Text)
+            //{
+            //    resultLabel.Text = "Yes!   They are equal!";
+            //}
+            //else
+            //{
+            //    resultLabel.Text = "No!  They are not equal!";
+            //}
+
+            /*if (coolCheckBox.Checked)
+            {
+                resultLabel.Text = "Yes, you are definitely cool.";
+            }
+            else
+            {
+                resultLabel.Text = "Don't be so hard on yourself.";
+            }
+            */
+
+            if (pizzaRadioButton.Checked)
+            {
+                resultLabel.Text = "You must be from Chicago!";
+            }
+            else if (crabLegsRadioButton.Checked)
+            {
+                resultLabel.Text = "You must be from Boston!";
+            }
+            else if (cheeseburgerRadioButton.Checked)
+            {
+                resultLabel.Text = "You must be an American!";
+            }
+            else
+            {
+                resultLabel.Text = "Please select one of the options.";
+            }
+        }
+    }
+}
